c86a71a7f737fe72ae47b6d6b641c3c3ad1a5536
Mampionona <rkthmamp@gmail.com>
2017-10-30 13:29:54 +0300

    revert

--- application/modules/user/views/sections/marketing_stats.php
+++
@@ -1,6 +0,0 @@
-<ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="<?php echo site_url('user/dashboard') ?>">Dashboard</a></li>
-    <li class="breadcrumb-item"><a href="<?php echo site_url('user/marketing/request') ?>">Marketing</a></li>
-    <li class="breadcrumb-item active">Statistique</li>
-</ol>
-<?php echo isset($output) ? $output : ''; ?>
